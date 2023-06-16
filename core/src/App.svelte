36412e19da222977b8b4ae731c1aa0996bdc544a
Terrell-byte <danieltsutton@outlook.com>
2023-06-16 16:10:30 +0200

    added tailwind correctly to project

--- core/src/App.svelte
+++ core/src/App.svelte
@@ -1,8 +1,9 @@
 <script>
+    import TailwindCSS from './app.css';
 
+    
 </script>
 
-<main>
-    <h1>App</h1>
-    <p>Start editing to see some magic happen :)</p>
-</main>
+<main class="flex items-center justify-center bg-#FFFCF2" style="height: 50vh;">
+    <h1 class="text-4xl font-bold">Welcome to my website</h1>
+  </main>
